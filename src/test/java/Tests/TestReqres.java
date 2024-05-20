b5aabafb5d3daa5417f69e4bd11253d8a1cffb59
Andrei Bodrug <andrei.bodrug@yahoo.com>
2024-05-20 13:22:42 +0300

    First commit of API testing

---
+++ src/test/java/Tests/TestReqres.java
@@ -0,0 +1,54 @@
+package Tests;
+
+
+import io.qameta.allure.Allure;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static utils.Steps.*;
+
+public class TestReqres {
+
+    @BeforeSuite
+    public void testSetUp() {
+
+        baseURI = "https://reqres.in/API";
+    }
+
+    @Test
+    public void testListUsers(){
+        String url = "/users?page=2";
+
+        Response response = GET(url);
+
+        isStatusCodeValid(response, 200);
+
+        /*given()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .assertThat()
+                .statusCode(200);*/
+
+    }
+    @Test
+    public void testSingleUser(){
+
+        String url = "/users/2";
+        Response response = GET(url);
+        isStatusCodeValid(response, 200);
+
+    }
+    @Test
+    public void testCreate(){
+        String url = "/users";
+        String body = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\"\n" +
+                "}";
+        Response response = POST(body,url);
+        isStatusCodeValid(response, 201);
+    }
+}
